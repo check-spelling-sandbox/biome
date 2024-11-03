1183feaa7f15633d70a1f7fe5d17ddf73f2bffcc
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-11-02 21:17:11 -0400

    spelling: graphql
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- crates/biome_cli/src/commands/rage.rs
+++ crates/biome_cli/src/commands/rage.rs
@@ -294,13 +294,13 @@ impl Display for RageConfiguration<'_, '_> {
                         let javascript_linter = configuration.get_javascript_linter_configuration();
                         let json_linter = configuration.get_json_linter_configuration();
                         let css_linter = configuration.get_css_linter_configuration();
-                        let graphq_linter = configuration.get_graphql_linter_configuration();
+                        let graphql_linter = configuration.get_graphql_linter_configuration();
                         markup! (
                             {Section("Linter")}
                             {KeyValuePair("JavaScript enabled", markup!({DebugDisplay(javascript_linter.enabled)}))}
                             {KeyValuePair("JSON enabled", markup!({DebugDisplay(json_linter.enabled)}))}
                             {KeyValuePair("CSS enabled", markup!({DebugDisplay(css_linter.enabled)}))}
-                            {KeyValuePair("GraphQL enabled", markup!({DebugDisplay(graphq_linter.enabled)}))}
+                            {KeyValuePair("GraphQL enabled", markup!({DebugDisplay(graphql_linter.enabled)}))}
                             {KeyValuePair("Recommended", markup!({DebugDisplay(linter_configuration.recommended.unwrap_or_default())}))}
                             {KeyValuePair("All", markup!({DebugDisplay(linter_configuration.all.unwrap_or_default())}))}
                             {RageConfigurationLintRules("Enabled rules", linter_configuration)}
